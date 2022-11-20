070c58b95b57102cd9dd1df806379fc56395af67
胡仁祥 <18917734365@163.com>
2022-11-20 10:53:15 +0800

    System.arrayCopy用法

---
+++ src/main/java/com/hurenxiang/basicKnowlodge/arrayCopyTest.java
@@ -0,0 +1,12 @@
+package com.hurenxiang.basicKnowlodge;
+
+import java.util.Arrays;
+
+public class arrayCopyTest {
+    public static void main(String[] args) {
+        int[] origin = {1,2,4};
+        int[] t = new int[8];
+        System.arraycopy(origin,0,t,4,2);
+        System.out.println(Arrays.toString(t));
+    }
+}
