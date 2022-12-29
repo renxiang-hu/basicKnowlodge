0b49ccfcf96b99b79aceec833b14e81667213eb1
胡仁祥 <18917734365@163.com>
2022-12-29 09:45:15 +0800

    动态代理和普通创建

--- src/main/java/com/hurenxiang/designModel/proxy/static_proxy/Client.java
+++ src/main/java/com/hurenxiang/designModel/proxy/static_proxy/Client.java
@@ -2,7 +2,9 @@ package com.hurenxiang.designModel.proxy.static_proxy;
 
 public class Client {
     public static void main(String[] args) {
+        //创建代售点类对象
         ProxyPoint proxyPoint = new ProxyPoint();
+        //调用方法进行买票
         proxyPoint.sell();
     }
 }
